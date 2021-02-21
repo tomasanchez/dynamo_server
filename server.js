0e1db6ac4ed1eb621ef9fcbf5a93da5ed646ba44
Tomas <tosanchez@frba.utn.edu.ar>
2021-02-21 16:09:36 -0300

    perf: add connection error handling

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.log(err));
 const connection = mongoose.connection;
 
 connection.once("open", () => {
